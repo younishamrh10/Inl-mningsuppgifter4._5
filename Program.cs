6952f31bf665f8870388a0f53858c7cd9204be03
younishamrh10 <yoha06001@utb.vaxjo.se>
2023-10-23 15:27:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Inlämningsuppgifter4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal: ");
+            string heltal = Console.ReadLine();
+            string nyttHeltal = "";
+
+            for (int i = 0; i < heltal.Length; i++)
+            {
+                char tecken = heltal[i];
+
+                if (char.IsDigit(tecken))
+                {
+                    int siffra = (int)char.GetNumericValue(tecken);
+                    siffra = (siffra + 1) % 10;
+                    nyttHeltal += siffra;
+                }
+                else
+                {
+                    nyttHeltal += tecken;
+                }
+            }
+
+            Console.WriteLine("Det nya talet är: " + nyttHeltal);
+        }
+    }
+
+}
